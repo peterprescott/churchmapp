4319dcd0c42458a424745731f2a2a6dacb89f2ed
Peter Prescott <prescott.peter@gmail.com>
2020-05-03 01:23:51 +0100

    start implementing @aadibajpai 's pythonanywhere/github continual integration solution

--- backend/run.py
+++ backend/run.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_cors import CORS
+import git
 
 app = Flask(__name__)
 CORS(app)
@@ -21,6 +22,17 @@ from resources import ConverterResource
 api.add_resource(ConverterResource, '/convert/<string:postcode>', endpoint='coords')
 api.add_resource(TodoListResource, '/todos', endpoint='todos')
 api.add_resource(TodoResource, '/todos/<string:id>', endpoint='todo')
+
+@app.route('/git', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/peterprescott/flask-vue-app')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
