fca035b11fe973d959db39e6d15aa936d4aa1ebd
Peter Prescott <prescott.peter@gmail.com>
2020-05-03 02:43:25 +0100

    another tweak to test whether it's working yet

--- backend/run.py
+++ backend/run.py
@@ -36,8 +36,8 @@ def webhook():
 
 @app.route('/test')
 def test_python_anywhere():
-    # it might be working now
-    return 'a different measure of success'
+    # making gentle progress
+    return 'enjoy the journey'
 
 if __name__ == '__main__':
     app.run(debug=True)
