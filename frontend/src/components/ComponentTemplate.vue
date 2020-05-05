4c72e2e1ba29b2eb81563d561e550cc607f7502a
Peter Prescott <prescott.peter@gmail.com>
2020-05-05 09:34:56 +0100

    add generic ComponentTemplate

---
+++ frontend/src/components/ComponentTemplate.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+		<p> {{ variableName }} </p>
+	</div>
+</template>
+
+<script>
+/* import ; */
+
+/* const x = y; */
+
+export default {
+  name: 'MyComponent',
+	components: {
+	  ,
+	},
+
+	props: {
+	/**
+	  * Doc for property
+	*/
+    aProperty: {
+  		},
+	},
+
+	data() {
+	  return: {
+		  variableName: 'value',
+	  };
+	
+	},
+
+	computed: {
+    computedVariable: function() {
+		  return this.variableName.reverse() // or whatever
+			}
+
+	},
+
+	beforeDestroy() {
+	  console.log('Do something *beforeDestroy*');
+	},
+
+	mounted() {
+	  console.log('Do something when mounted.');
+	},
+
+	methods: {
+    someFunction() {
+		  alert('Do something when called');
+		},
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
+
+<docs>
+
+</docs>
