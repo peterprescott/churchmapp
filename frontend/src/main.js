26a783fb265c374883cbdafa560e7bc3ca0226d0
Peter Prescott <prescott.peter@gmail.com>
2020-05-05 10:38:22 +0100

    make icons show (bug-fix ~ cf. github.com/vue-leaflet/Vue2Leaflet/issues/103)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,3 +8,10 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+delete L.Icon.Default.prototype._getIconUrl  
+L.Icon.Default.mergeOptions({  
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),  
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),  
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')  
+})
