5e00521bb8cdabbac1dd4543750314c7cd1a2677
Peter Prescott <prescott.peter@gmail.com>
2020-05-15 06:22:33 +0100

    change api from local to test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-		<LoginNav />
 		<LeafletMap 
-			apiURL="http://localhost:5000"
+			apiURL="https://00churchmapp.pythonanywhere.com"
 		  />
   </div>
 </template>
 
 <script>
-/* import LoginNav from './components/LoginNav.vue' */
 import LeafletMap from './components/LeafletMap.vue'
 
 export default {
   name: 'App',
   components: {
-		/* LoginNav, */
     LeafletMap,
   }
 }
