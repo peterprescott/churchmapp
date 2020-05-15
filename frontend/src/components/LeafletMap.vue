3a1ea66b0c5516420127ac8004be0cb7fa63d3e0
Peter Prescott <prescott.peter@gmail.com>
2020-05-15 12:38:48 +0100

    alert auth failure

--- frontend/src/components/LeafletMap.vue
+++ frontend/src/components/LeafletMap.vue
@@ -220,6 +220,7 @@ export default {
 		authFail() {
 			this.token='';
 			this.connected = false;
+			alert('Authentication failed! Did you enter your email and password correctly?')
 			console.log('Failed.')
 		},
        
